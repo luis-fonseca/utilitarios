1fad3ba4c96fef98a43e6a189c99742277b43f9c
Luís Fonseca <fnsc.luis@gmail.com>
2017-10-12 00:35:42 -0300

    Aprimorado a forma para gerar nomes aleatórios e quase finalizado a forma de criar pastas randomicamente em diversos níveis

--- python/criar_arquivos_diretorios_aleatorios/test.py
+++ python/criar_arquivos_diretorios_aleatorios/test.py
@@ -1,9 +1,40 @@
-def print_msg(msg, attempts):
+import uuid             # gerar identificador unico
+import subprocess       # chamar programa externo fsutil
+import os
+import binascii
 
-    if attempts < 3:
-        print(msg)
-        return print_msg(msg, attempts + 1)
+def generate_random_name():
+    """
+        a função retorna uma sequencia aleatoria de caracteres
+    """
+    return uuid.uuid4().hex[0:8]
 
 
-print_msg('ola', 0)
+# dirs = []
 
+# start_level = 0
+# final_level = 3
+# dir_count = 2
+
+# base_dir = os.getcwd()
+# output_dir = os.path.join(base_dir, 'saida')
+
+# for level in range(start_level, final_level):
+#     len_dir_a = len(dir_a)
+#     if len_dir_a == 0:
+#         for j in range(0, dir_count):
+#             dir_a.insert(0, os.path.relpath(generate_random_name(), output_dir))
+#     else:
+#         dirs_a_temp = []
+#         for j in dir_a:
+#             for k in range(0, dir_count):
+#                 dir_a_temp.insert(0, os.path.join(j, generate_random_name()))
+
+#         dir_a.clear()
+#         dir_a = dir_a_temp[:]
+
+
+#print(dir_a)
+#print(dir_a_temp)
+
+print(binascii.b2a_hex(os.urandom(4)))
