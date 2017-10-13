44a190172c64bd5373924ead88b02996a31498a8
Luís F <fnsc.luis@gmail.com>
2017-10-13 02:25:57 -0300

    criado função para gerar nomes validos no ambiente windows

--- python/criar_arquivos_diretorios_aleatorios/test.py
+++ python/criar_arquivos_diretorios_aleatorios/test.py
@@ -2,13 +2,41 @@ import uuid             # gerar identificador unico
 import subprocess       # chamar programa externo fsutil
 import os
 import binascii
+import random
+import string
+import platform
 
-def generate_random_name():
-    """
-        a função retorna uma sequencia aleatoria de caracteres
-    """
-    return uuid.uuid4().hex[0:8]
+# alfa, num, alfanum, windows_chars, linux_chars
 
+def generate_random_name(character_set, size):
+    """
+        a função retorna uma sequencia aleatoria de caracteres para nomear arquivos e
+        diretórios
+        character_set: 1 -> alfa, 2 -> num, 3 -> alfanum, 4 -> linux, 5 -> windows
+    """
+    name = ''
+    if character_set == 'alfa':
+        name = random.sample(string.ascii_letters, size)
+    elif character_set == 'num':
+        name = random.sample(string.digits, size)
+    elif character_set == 'alfanum':
+        characters = string.ascii_letters + string.digits
+        name = random.sample(characters, size)
+    elif character_set == 'linux':
+        pass
+    else:
+        pass
+
+
+#print(string.printable)
+
+#print(binascii.b2a_hex(os.urandom(4)))
+
+#for l in range('')
+#print(string.ascii_letters)
+#print(random.sample(string.ascii_letters, 8))
+
+#print(random.SystemRandom().randint(1))
 
 # dirs = []
 
@@ -37,4 +65,45 @@ def generate_random_name():
 #print(dir_a)
 #print(dir_a_temp)
 
-print(binascii.b2a_hex(os.urandom(4)))
+#print(string.punctuation)
+#invalid_chars = list(set(string.punctuation) - set('\/:*?"<>|'.split()))
+
+ #. "
+
+characters = string.ascii_letters + string.digits + "{['$-=};_`,!()~&+]%^@#"
+
+count = 1
+total_chars = 8
+name = ""
+i = 0
+
+while i <= 50000:
+    while count <= total_chars:
+        if count > 1 and count < total_chars:
+            name += ''.join(random.sample(characters + ". ", 1))
+        else:
+            name += ''.join(random.sample(characters, 1))
+
+        count += 1
+
+    if name[0] in " ." or name[7] in " .":
+        print(name)
+        print('encontrou')
+        break
+
+    print(i, name)
+
+    name = ""
+    count = 1
+
+    i += 1
+
+#s = ' '
+#s = 'a'
+#print(s + ''.join(random.sample(characters + ". ", 1)))
+#print(s[0] in '. ')
+#print(string.ascii_letters)
+#str.join()
+#print(string.jo(g1 - g2))
+#print(invalid_chars)
+#print(string.ascii_letters + string.digits)
